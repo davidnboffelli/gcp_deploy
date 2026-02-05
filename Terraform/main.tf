9f11937bd2daccc4e03fd7ee732f6fd0ab1fa880
davidnboffelli <166407728+davidnboffelli@users.noreply.github.com>
2026-02-05 18:03:14 +0100

    Chore: fix

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,13 +2,14 @@ module "project-factory" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 18.2"
 
-  name                 = "prj-test-1"
-  random_project_id    = false
-  org_id               = "577081811435"
-  folder_id            = "414750509829"
+  name                      = "prj-test-1"
+  random_project_id         = false
+  org_id                    = "577081811435"
+  folder_id                 = "414750509829"
 #   usage_bucket_name    = "pf-test-1-usage-report-bucket"
 #   usage_bucket_prefix  = "pf/test/1/integration"
-  billing_account      = "014F7F-035A51-0DB60B"
+  billing_account           = "014F7F-035A51-0DB60B"
+  default_service_account   = "delete"
 #   svpc_host_project_id = "shared_vpc_host_name"
     
 #   shared_vpc_subnets = [
