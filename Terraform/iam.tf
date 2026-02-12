63fea65a4ec62a30af39fa81b28757fb46d7e77b
davidnboffelli <166407728+davidnboffelli@users.noreply.github.com>
2026-02-12 14:02:03 +0100

    Limpieza

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -1,38 +1,38 @@
-module "iam" {
-  # depends_on = [ google_project.doer_projects ]
-  source = "git@github.com:davidnboffelli/terraform-google-iam.git//?ref=main"
-  # for_each = local.doers_and_values
+# module "iam" {
+#   # depends_on = [ google_project.doer_projects ]
+#   source = "git@github.com:davidnboffelli/terraform-google-iam.git//?ref=main"
+#   # for_each = local.doers_and_values
 
-  # service_accounts = {
-  #   "sa-${each.key}" = {
-  #     service_account_id    = "sa-${each.key}"
-  #     project_id            = "${each.value.prj_cross_id}"
-  #     description           = "SA para uso del Doer ${each.key} en el bootcamp de GCP"
-  #   },
-  # }
+#   # service_accounts = {
+#   #   "sa-${each.key}" = {
+#   #     service_account_id    = "sa-${each.key}"
+#   #     project_id            = "${each.value.prj_cross_id}"
+#   #     description           = "SA para uso del Doer ${each.key} en el bootcamp de GCP"
+#   #   },
+#   # }
 
-  # roles_assignation = {
-  #   ###############SERVICE ACCOUNTS & SERVICE AGENTS###############
-  #   "newServiceAccount:sa-${each.key}" = {
-  #     project_level_roles = [
-  #       {
-  #         project_id = "${each.value.prj_cross_id}"
-  #         roles = [
-  #           "roles/owner",
-  #         ]
-  #       },
-  #     ]
-  #   },
-  # }
+#   # roles_assignation = {
+#   #   ###############SERVICE ACCOUNTS & SERVICE AGENTS###############
+#   #   "newServiceAccount:sa-${each.key}" = {
+#   #     project_level_roles = [
+#   #       {
+#   #         project_id = "${each.value.prj_cross_id}"
+#   #         roles = [
+#   #           "roles/owner",
+#   #         ]
+#   #       },
+#   #     ]
+#   #   },
+#   # }
 
-  groups = {
-    "gcp-test-module-iam" = {
-      name        = "gcp-test-module-iam"
-      description = "Grupo de Prueba"
-      type        = "default"
-      members = [
-        "dnazareno@stemdo.io",
-      ]
-    }
-  }
-}
+#   # groups = {
+#   #   "gcp-test-module-iam" = {
+#   #     name        = "gcp-test-module-iam"
+#   #     description = "Grupo de Prueba"
+#   #     type        = "default"
+#   #     members = [
+#   #       "dnazareno@stemdo.io",
+#   #     ]
+#   #   }
+#   # }
+# }
